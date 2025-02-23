8cb1a885b9c1882b0a1fafb78fe55447c0fe8ba3
Prince Muel <vansomecsam@gmail.com>
2025-02-23 04:32:58 +0100

    feat(005): start Anagram exercise (rs)

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,5 +1,8 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(
+    word: &str,
+    possible_anagrams: &[&str],
+) -> HashSet<&'a str> {
+    HashSet::from_iter([""])
 }
