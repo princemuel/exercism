d8166eec7af45e383247f118fbfdd065e7057879
Prince Muel <vansomecsam@gmail.com>
2025-02-26 03:00:39 +0100

    feat(007): complete Sublist exercise (rs)

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -6,8 +6,22 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist(first_list: &[i32], second_list: &[i32]) -> Comparison {
-    todo!(
-        "Determine if the {first_list:?} is equal to, sublist of, superlist of or unequal to {second_list:?}."
-    );
+pub fn sublist<T>(first: &[T], second: &[T]) -> Comparison
+where
+    T: PartialEq,
+{
+    use Comparison::*;
+
+    let is_sublist = |small: &[T], big: &[T]| {
+        big.windows(small.len()).any(|window| window == small)
+    };
+
+    match (first, second) {
+        _ if first == second => Equal,
+        _ if first.is_empty() => Sublist,
+        _ if second.is_empty() => Superlist,
+        _ if is_sublist(second, first) => Superlist,
+        _ if is_sublist(first, second) => Sublist,
+        _ => Unequal,
+    }
 }
