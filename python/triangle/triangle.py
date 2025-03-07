e23efb4439d6e3bc836e64a404df64f76280e1b5
Prince Muel <vansomecsam@gmail.com>
2025-03-07 03:38:48 +0100

    feat(py): {020} complete Triangle exercise [2nd]

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,22 +1,25 @@
-from typing import List
+from typing import Callable, List
 
 Sides = List[int | float]
 
 
+def is_triangle(func: Callable) -> Callable:
+    def inner(sides: Sides):
+        return sum(sides) > 2 * max(sides) and func(sides)
+
+    return inner
+
+
+@is_triangle
 def equilateral(sides: Sides):
-    a, b, c = sides
-    return is_triangle(sides) and (a == b == c)
+    return len(set(sides)) == 1
 
 
+@is_triangle
 def isosceles(sides: Sides):
-    a, b, c = sides
-    return is_triangle(sides) and (a == b or a == c or b == c)
+    return len(set(sides)) < 3
 
 
+@is_triangle
 def scalene(sides: Sides):
-    return is_triangle(sides) and not isosceles(sides)
-
-
-def is_triangle(sides: Sides):
-    a, b, c = sides
-    return (a + b > c) and (a + c > b) and (b + c > a)
+    return len(set(sides)) == 3
