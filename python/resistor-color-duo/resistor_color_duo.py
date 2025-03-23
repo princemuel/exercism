97ec0ac9b201c811eda52f191a332be9cdade801
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-03-23 05:12:58 +0100

    style: format files

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,15 +1,16 @@
-
-COLORS =  [ "black",
-            "brown",
-            "red",
-            "orange",
-            "yellow",
-            "green",
-            "blue",
-            "violet",
-            "grey",
-            "white", ]
+COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
 
 
 def value(colors):
-    return COLORS.index(colors[0])*10 + COLORS.index(colors[1])
+    return COLORS.index(colors[0]) * 10 + COLORS.index(colors[1])
