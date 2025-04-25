8c6f2585f299cb914938af18a81d6019315319fe
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-04-25 02:08:17 +0100

    feat(ts): 029 Reverse String

---
+++ typescript/reverse-string/reverse-string.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "@jest/globals"
+import { reverse } from "./reverse-string.ts"
+
+describe("Reverse String", () => {
+    it("an empty string", () => {
+        const expected = ""
+        expect(reverse("")).toEqual(expected)
+    })
+
+    it("a word", () => {
+        const expected = "tobor"
+        expect(reverse("robot")).toEqual(expected)
+    })
+
+    it("a capitalized word", () => {
+        const expected = "nemaR"
+        expect(reverse("Ramen")).toEqual(expected)
+    })
+
+    it("a sentence with punctuation", () => {
+        const expected = `!yrgnuh m'I`
+        expect(reverse(`I'm hungry!`)).toEqual(expected)
+    })
+
+    it("a palindrome", () => {
+        const expected = "racecar"
+        expect(reverse("racecar")).toEqual(expected)
+    })
+})
