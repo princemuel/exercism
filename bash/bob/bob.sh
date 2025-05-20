b8fe69fa161171cb076122795bf3b3d5510e7ef2
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-05-20 13:29:53 +0100

    feat(bash): 009 Bob

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+# Get the input from the first argument or read from stdin if no argument is provided
+if [ $# -eq 0 ]; then
+    read -r input
+else
+    input="$1"
+fi
+
+# Remove any trailing/leading whitespace
+input=$(echo "$input" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+# Check if the input is silence (empty or only whitespace)
+if [ -z "$input" ]; then
+    echo "Fine. Be that way!"
+    exit 0
+fi
+
+# Check if the input is a question (ends with a question mark)
+is_question=0
+if [[ "$input" == *"?" ]]; then
+    is_question=1
+fi
+
+# Check if the input is yelling (contains letters and all uppercase)
+is_yelling=0
+if [[ "$input" =~ [A-Z] && ! "$input" =~ [a-z] ]]; then
+    is_yelling=1
+fi
+
+# Determine Bob's response based on the input type
+if [ $is_question -eq 1 ] && [ $is_yelling -eq 1 ]; then
+    echo "Calm down, I know what I'm doing!"
+elif [ $is_question -eq 1 ]; then
+    echo "Sure."
+elif [ $is_yelling -eq 1 ]; then
+    echo "Whoa, chill out!"
+else
+    echo "Whatever."
+fi
