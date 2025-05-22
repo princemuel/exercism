93665ac0818efcb1abb6fae017523fa9a2be2cfb
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-05-22 05:04:36 +0100

    feat(ts): 007 RNA Transcription

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,16 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
-}
+export const toRna = (strand: string): string => {
+    const complements = new Map([
+        ["G", "C"],
+        ["C", "G"],
+        ["T", "A"],
+        ["A", "U"],
+    ]);
+
+    return [...strand]
+        .map((nucleotide) => {
+            const rna = complements.get(nucleotide);
+            if (!rna) throw new Error("Invalid input DNA.");
+            return rna;
+        })
+        .join("");
+};
