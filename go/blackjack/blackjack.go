e1e002dd6d4bd54f6fb4f98d7d861a7e5ccefdb3
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-06-09 03:36:34 +0100

    feat(go): 006 Blackjack

--- go/blackjack/blackjack.go
+++ go/blackjack/blackjack.go
@@ -1,37 +1,49 @@
 package blackjack
 
+var cardValues = map[string]int{
+	"ace":   11,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+	"ten":   10,
+	"jack":  10,
+	"queen": 10,
+	"king":  10,
+}
+
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	switch card {
-	case "king", "queen", "jack", "ten":
-		return 10
-	case "ace":
-		return 11
-	case "nine":
-		return 9
-	case "eight":
-		return 8
-	case "seven":
-		return 7
-	case "six":
-		return 6
-	case "five":
-		return 5
-	case "four":
-		return 4
-	case "three":
-		return 3
-	case "two":
-		return 2
-	default:
-		return 0
+	if value, exists := cardValues[card]; exists {
+		return value
 	}
+	return 0
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	total := ParseCard(card1) + ParseCard(card2)
+	if card1 == "ace" && card2 == "ace" {
+		return "P"
+	}
 
-	return ""
+	c1, c2, dealer := ParseCard(card1), ParseCard(card2), ParseCard(dealerCard)
+	playerScore := c1 + c2
+
+	if playerScore == 21 {
+		return map[bool]string{true: "S", false: "W"}[dealer >= 10]
+	}
+
+	switch {
+	case playerScore >= 17:
+		return "S"
+	case playerScore >= 12:
+		return map[bool]string{true: "H", false: "S"}[dealer >= 7]
+	default:
+		return "H"
+	}
 }
