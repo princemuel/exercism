159a9e94c35063c1025d7712d70ad4e7b9e32821
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-06-19 03:49:38 +0100

    feat(rs): solve exercise `Reverse String`

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -5,3 +5,8 @@ use unicode_segmentation::UnicodeSegmentation;
 pub fn reverse(input: &str) -> String {
     input.graphemes(true).rev().collect()
 }
+
+
+pub fn reverse_bonus(input: &str) -> String {
+    input.chars().rev().collect()
+}
