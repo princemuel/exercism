0cb72c44f1892e62eb7c0db5199bc5de096f9491
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-06-19 03:39:52 +0100

    feat(go): solve exercise `Annalyns Infiltration`

--- go/annalyns-infiltration/annalyns_infiltration.go
+++ go/annalyns-infiltration/annalyns_infiltration.go
@@ -18,6 +18,5 @@ func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
 // CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
 // or if Annalyn's pet dog is with her and the archer is sleeping.
 func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
-	return (petDogIsPresent && !archerIsAwake) || (prisonerIsAwake && !knightIsAwake && !archerIsAwake)
-
+    return !archerIsAwake && (petDogIsPresent || (prisonerIsAwake && !knightIsAwake))
 }
