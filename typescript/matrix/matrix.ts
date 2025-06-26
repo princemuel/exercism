308f8dac692b8f056f03dca932c21c01247d9917
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-06-26 02:27:02 +0100

    feat(ts): attempt `Matrix`

---
+++ typescript/matrix/matrix.ts
@@ -0,0 +1,13 @@
+export class Matrix {
+    constructor(value: string) {
+        throw new Error("Remove this statement and implement this function");
+    }
+
+    get rows(): number[] {
+        return [];
+    }
+
+    get columns(): number[] {
+        return [];
+    }
+}
