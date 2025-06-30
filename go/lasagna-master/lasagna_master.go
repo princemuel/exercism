c2b5d6964692b8873f6ffff1dfdc5bf33fa50f37
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-06-30 06:32:40 +0100

    feat(go): solve `Lasagna Master`

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,18 +1,39 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, minutesPerLayer int) int {
+	if minutesPerLayer == 0 {
+		minutesPerLayer = 2
+	}
+	return len(layers) * minutesPerLayer
+}
 
-// TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+	var noodles int
+	var sauce float64
 
-// TODO: define the 'AddSecretIngredient()' function
+	for _, layer := range layers {
+		switch layer {
+		case "noodles":
+			noodles += 50
+		case "sauce":
+			sauce += 0.2
+		}
+	}
 
-// TODO: define the 'ScaleRecipe()' function
+	return noodles, sauce
+}
 
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
+func AddSecretIngredient(friendsList []string, myList []string) {
+	myList[len(myList)-1] = friendsList[len(friendsList)-1]
+}
+
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	scaled := make([]float64, len(quantities))
+	ratio := float64(portions) / 2.0
+
+	for i, quantity := range quantities {
+		scaled[i] = quantity * ratio
+	}
+
+	return scaled
+}
