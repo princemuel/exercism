c7a15cac0c810a6c6c97e0f48136d0441ca609ae
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-06-30 05:24:13 +0100

    feat(csharp): solve `Lucians Luscious Lasagna`

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,10 @@
 class Lasagna
 {
-    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven() => 40;
 
-    // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int minutesInOven) => ExpectedMinutesInOven() - minutesInOven;
 
-    // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int numberOfLayers) => numberOfLayers * 2;
 
-    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int numberOfLayers, int minutesInOven) => PreparationTimeInMinutes(numberOfLayers) + minutesInOven;
 }
