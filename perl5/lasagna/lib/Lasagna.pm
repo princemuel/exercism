94760c77b2b8aa701a12567721a9ca57097e45ae
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-06-30 05:39:12 +0100

    feat(perl5): attempt `Lucians Luscious Lasagna`

---
+++ perl5/lasagna/lib/Lasagna.pm
@@ -0,0 +1,19 @@
+package Lasagna;
+
+use v5.40;
+
+our $ExpectedMinutesInOven = undef;
+
+sub remaining_minutes_in_oven ($actual_minutes_in_oven) {
+}
+
+sub preparation_time_in_minutes ($number_of_layers) {
+}
+
+sub total_time_in_minutes ( $number_of_layers, $actual_minutes_in_oven ) {
+}
+
+sub oven_alarm () {
+}
+
+1;
