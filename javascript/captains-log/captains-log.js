dc2258b2d05aa256b9ef1a4b3e4e2a3f3e346214
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-07-01 13:20:55 +0100

    feat(javascript): solve `Captain's Log`

--- javascript/captains-log/captains-log.js
+++ javascript/captains-log/captains-log.js
@@ -5,22 +5,20 @@
  *
  * @returns {string} the generated registry number.
  */
-export const randomShipRegistryNumber = () => `NCC-${Math.floor(Math.random() * 9000) + 1000}`;
+export const randomShipRegistryNumber = () => `NCC-${Math.floor(1000 + Math.random() * 9000)}`;
 
 /**
  * Generates a random stardate.
  *
  * @returns {number} a stardate between 41000 (inclusive) and 42000 (exclusive).
  */
-export const randomStardate = () => Math.random() * 1000 + 41000;
+export const randomStardate = () => 41000 + Math.random() * 1000;
+
+const PLANET_CLASSES = "DHJKLMNRTY";
 
 /**
  * Generates a random planet class.
  *
  * @returns {string} a one-letter planet class.
  */
-export function randomPlanetClass() {
-    const classes = "DHJKLMNRTY";
-    const randomIdx = Math.floor(Math.random() * classes.length);
-    return classes[randomIdx];
-}
+export const randomPlanetClass = () => PLANET_CLASSES[Math.floor(Math.random() * PLANET_CLASSES.length)];
